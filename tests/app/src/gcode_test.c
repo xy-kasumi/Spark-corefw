7b89cc0d72982a58323bd3f7419e8d0f2de73546
xyx <xanxys@gmail.com>
2025-06-05 22:10:51 +0900

    Support automated real-board test w/ twister

---
+++ tests/app/src/gcode_test.c
@@ -0,0 +1,17 @@
+#include <zephyr/ztest.h>
+#include "gcode.h"
+
+ZTEST(gcode, test_basic_g0_command)
+{
+    gcode_parsed_t parsed;
+    bool result = parse_gcode("G0", &parsed);
+    
+    zassert_true(result, "G0 command should parse successfully");
+    zassert_equal(parsed.command, 0, "Command should be 0 for G0");
+    zassert_false(parsed.has_x, "Should not have X coordinate");
+    zassert_false(parsed.has_y, "Should not have Y coordinate");
+    zassert_false(parsed.has_z, "Should not have Z coordinate");
+    // zassert_true(parsed.has_z, "Should not have Z coordinate");
+}
+
+ZTEST_SUITE(gcode, NULL, NULL, NULL, NULL, NULL);
